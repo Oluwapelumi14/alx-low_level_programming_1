c03f434e4184abd365d20ad8e7cf89a15d215854
Akintoyehub01 <hakintoye14@gmail.com>
2022-04-11 06:54:26 -0700

    postive or negative

--- alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,44 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success/correct)
- */
-
-int main(void)
-{
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n == 0)
-{
-printf("%i is zero\n", n);
-}
-
-else if (n < 0)
-{
-printf("%i is negative\n", n);
-}
-
-else
-{
-printf("%i is positive\n", n);
-}
-
-return (0);
-
-}
-chmod u+x 0-positive_or_negative.c
-git add .
-git commit-m "positive and negative'
-git push
-
-
 
 #include <stdlib.h>
 #include <time.h>
@@ -75,5 +34,4 @@ printf("%i is positive\n", n);
 return (0);
 
 }
-
 
